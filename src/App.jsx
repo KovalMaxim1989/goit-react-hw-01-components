8a440a1137858a4eb6aa1f916a014e993f77925a
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-02-14 14:31:26 +0200

    1-2
    1-2

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Profile } from "./components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+
+import user from './data/user.json';
+import data from './data/data.json'
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
+  );
+};
